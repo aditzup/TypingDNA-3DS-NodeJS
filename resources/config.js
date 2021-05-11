8cf99a396e37c12497d03a1cba1c25ee64425a35
adrianpoacatypingdna <68328419+adrianpoacatypingdna@users.noreply.github.com>
2021-05-11 10:14:01 +0300

    Initial Commit

---
+++ resources/config.js
@@ -0,0 +1,11 @@
+
+module.exports =
+    config = {
+        typingDNA: {
+            apiKey: '',
+            apiSecret: ''
+        },
+        someprivatekey: '@2mws9~&%?+RN',
+        sessionSecret: 'asdkjkfh63ryfsdgcqg87w4rf7gas123',
+        sametext: `innovation service savings`,
+    };
